ddde7a1f04c5d350c5e14f37f69c194f05f0bae9
AlexanderAgathonov <a.a.agathonov@gmail.com>
2016-05-23 00:19:30 +0400

    django1

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
-gunicorn -c /home/box/web/etc/django.py wsgi --daemon
