477e69038319460bf6572c4b9dc86aa3daf424f5
AlexanderAgathonov <a.a.agathonov@gmail.com>
2016-05-22 23:36:38 +0400

    django1

--- etc/hello.py
+++ etc/hello.py
@@ -1,11 +1,2 @@
-CONFIG = {
-    'mode': 'wsgi',
-    'working_dir': '/home/box/web',
-    'args': (
-        '--bind=0.0.0.0:8080',
-        '--deamon',
-        '--workers=16',
-        '--timeout=60',
-        'hello:app',
-    ),
-}
+pythonpath = '/home/box/web'
+bind = "0.0.0.0:8080"
